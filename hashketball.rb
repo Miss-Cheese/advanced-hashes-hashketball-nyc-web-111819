6d35e302fa7666715aef40ac82c711ae97b4df01
Natalie Galligan <nat.galligan@gmail.com>
2019-11-16 18:20:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,28 +4,28 @@
        :team_name => "Brooklyn Nets",
        :colors => ["Black", "White"],
        :players => [{
-         :player_name
-         :number
-         :shoe
-         :points
-         :rebounds
-         :steals
-         :blocks
-         :slam_dunks
+         :player_name =>
+         :number =>
+         :shoe =>
+         :points =>
+         :rebounds =>
+         :steals =>
+         :blocks =>
+         :slam_dunks =>
          }]
      },
      :away => {
        :team_name => "Charlotte Hornets",
        :colors => ["Turquoise", "Purple"],
        :players => [{
-         :player_name
-         :number
-         :shoe
-         :points
-         :rebounds
-         :steals
-         :blocks
-         :slam_dunks
+         :player_name =>
+         :number =>
+         :shoe =>
+         :points =>
+         :rebounds =>
+         :steals =>
+         :blocks =>
+         :slam_dunks =>
          }]
      }
    }
